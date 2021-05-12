c4611c84f593d4c1c136451ed4c5cc7c4bd36d12
korobopolly <72500196+korobopolly@users.noreply.github.com>
2021-05-12 13:43:11 +0900

    first commit

---
+++ app/src/main/java/com/nugury/ark007/Frag1.java
@@ -0,0 +1,32 @@
+package com.nugury.ark007;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Frag1 extends Fragment {
+    private View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.frag1, container, false);
+
+        return view;
+    }
+}
