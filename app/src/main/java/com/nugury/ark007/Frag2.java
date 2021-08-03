158f6183831a9add337bf6f3c13dc1379d7d52de
DALDALGRAPH <72500196+korobopolly@users.noreply.github.com>
2021-08-03 11:35:51 +0900

    주사위 굴리기

--- app/src/main/java/com/nugury/ark007/Frag2.java
+++ app/src/main/java/com/nugury/ark007/Frag2.java
@@ -4,20 +4,42 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.Random;
+
 public class Frag2 extends Fragment {
     private View view;
+
+    TextView dicetext;
+    Button dicebutton;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.frag2,container,false);
+        View v = inflater.inflate(R.layout.frag2,container,false);
 
-        return view;
+        dicetext = v.findViewById(R.id.dicenum);
+        dicebutton = v.findViewById(R.id.diceroll);
 
+        dicebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int dicenum = 0;
+
+                Random random = new Random();
+
+                dicenum = random.nextInt(6)+1;
+
+                dicetext.setText(String.valueOf(dicenum));
+            }
+        });
+
+        return v;
     }
 }
