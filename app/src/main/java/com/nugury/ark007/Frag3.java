33b0693c37cfbb3fe79db38e2da77b5dd3d5559c
DALDALGRAPH <72500196+korobopolly@users.noreply.github.com>
2021-08-03 13:19:58 +0900

    GIF 재생

--- app/src/main/java/com/nugury/ark007/Frag3.java
+++ app/src/main/java/com/nugury/ark007/Frag3.java
@@ -4,20 +4,30 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
+
 public class Frag3 extends Fragment {
     private View view;
+
+    ImageView taeligif;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.frag3,container,false);
+        View v = inflater.inflate(R.layout.frag3,container,false);
 
-        return view;
+        taeligif = v.findViewById(R.id.gifimage);
+        GlideDrawableImageViewTarget gifImage = new GlideDrawableImageViewTarget(taeligif);
+        Glide.with(getContext()).load(R.drawable.yes).into(gifImage);
 
+        return v;
     }
 }
